b46384035745149301f10132a2592c1a5284c44a
Jonathan <mcjhona069@gmail.com>
2022-08-01 20:10:48 -0500

    feature 1

--- gh-next-actions-main/src/pages/index.js
+++ gh-next-actions-main/src/pages/index.js
@@ -1,7 +1,10 @@
 export default function Home() {
-  return (
-    <>
-      <h1>Hello Next.js</h1>
-    </>
-  );
+    const hola = 'hello'
+
+    return ( <
+        >
+        <
+        h1 > Hello Next.js < /h1 <
+        />
+    )
 }
