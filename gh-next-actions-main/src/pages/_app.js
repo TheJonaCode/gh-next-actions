fcb9cd0d04e613d78af905d3dff4c040e229ee9b
Jonathan <mcjhona069@gmail.com>
2022-08-01 20:33:22 -0500

    fix

--- gh-next-actions-main/src/pages/_app.js
+++ gh-next-actions-main/src/pages/_app.js
@@ -1,9 +1,8 @@
 import 'tailwindcss/tailwind.css';
 
-function MyApp({ Component, pageProps }
-    error!) {
+function MyApp({ Component, pageProps }) {
     return <Component {...pageProps }
-    />
+    />;
 }
 
-export default MyApp:
+export default MyApp;
